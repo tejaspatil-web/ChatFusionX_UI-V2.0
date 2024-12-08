4ce2704aa7a517dff453699d2df28c1c8ed0d31a
Tejas Patil <tejasp094@gmail.com>
2024-12-08 23:25:55 +0530

    added code for socket connection

---
+++ src/app/connection/socket.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  private socket: Socket;
+  private serverUrl: string = 'http://localhost:8080/getway';
+  private serverApiUrl: string = 'http://localhost:8080/api/v1/user';
+
+  constructor(private _http: HttpClient) {
+    this.socket = io(this.serverUrl);
+  }
+
+  // Join a group (room)
+  joinGroup(groupId: string): void {
+    this.socket.emit('joinGroup', groupId);
+  }
+
+  // Leave a group (room)
+  leaveGroup(groupName: string): void {
+    this.socket.emit('leaveGroup', groupName);
+  }
+
+  // Send a message to a group
+  sendMessageToGroup(groupId: string, message: any): void {
+    this.socket.emit('groupMessage', { groupId, message: message });
+  }
+
+  // Listen for responses from the server
+  onMessageReceived(groupId, callback: (data: any) => void): void {
+    this.socket.on(groupId, callback);
+  }
+
+  // Disconnect from the server
+  disconnect(): void {
+    this.socket.disconnect();
+  }
+}
